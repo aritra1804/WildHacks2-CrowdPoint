761e31af3bd3a78d7805255d9940e9ae105ec2d2
AKSHAT20-OFFICIAL <72640097+AKSHAT20-OFFICIAL@users.noreply.github.com>
2022-08-06 14:23:57 +0530

    changes

---
+++ crowdpoint_web/src/components/Welcome.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className="rounded  m-5 items-center-center ">
+          
+          
+    <p className="font-bold  px-5  text-lg">
+      Welcome to Crowd Point, a platform for ......
+    </p>
+    <p className="p-5  leading-loose font-serif ">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant. Mi in nulla posuere sollicitudin. Egestas quis ipsum suspendisse ultrices. Pretium aenean pharetra magna ac placerat vestibulum lectus. Nunc sed velit dignissim sodales ut. Turpis egestas integer eget aliquet nibh praesent tristique magna sit. Tristique sollicitudin nibh sit amet commodo. Sit amet dictum sit amet justo donec. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. In dictum non consectetur a erat nam at lectus urna.
+
+Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Morbi quis commodo odio aenean sed adipiscing. Aliquet nec ullamcorper sit amet risus nullam. Pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Ullamcorper velit sed ullamcorper morbi tincidunt. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Proin fermentum leo vel orci porta non pulvinar. Vulputate dignissim suspendisse in est ante in. Nec feugiat in fermentum posuere urna nec tincidunt. Diam vulputate ut pharetra sit amet aliquam. Pellentesque massa placerat duis ultricies lacus sed. Sem viverra aliquet eget sit amet tellus.
+    </p>
+    <div className="flex justify-center">
+    <p className="font-extrabold p-4 w-4/6 text-center ml-7 bg-gray-200 border border-red-400  rounded-full hover:bg-red-500   text-lg">
+      Download Now 
+    </p></div>
+    
+    
+  
+</div>
+  )
+}
+
+export default Welcome
